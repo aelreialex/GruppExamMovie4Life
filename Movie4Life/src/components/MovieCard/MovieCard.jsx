b81fc881aafcfb3007952f26171e99ffc2c5742a
Alexander <3kdyhfe25.erikal@folkuniversitetet.nu>
2026-04-13 15:21:00 +0200

    LandingPage/movieCard klar

--- Movie4Life/src/components/MovieCard/MovieCard.jsx
+++ Movie4Life/src/components/MovieCard/MovieCard.jsx
@@ -1,9 +1,17 @@
 import "./movieCard.css"
+import { useNavigate } from "react-router-dom"
 
 const MovieCard = ({movie}) => {
+  const navigate = useNavigate();
+  console.log(movie.imdbID)
+
+  function handleSinglePage(){
+  navigate(`movie/${movie.imdbID}`, {replace: true})
+}
+
   return (
     <article className="movieCard" id={movie.imdbID}>
-      <img className="movieCard__poster" src={movie.Poster}></img>
+      <img onClick={handleSinglePage} className="movieCard__poster" src={movie.Poster}></img>
       <h3 className="movieCard__title">{movie.Title}</h3>
     </article>
 
