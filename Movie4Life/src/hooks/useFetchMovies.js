ba8123d66924075646e9c2cd72d0dc6da794c99e
Boss <chaiyapat_ass@hotmail.com>
2026-04-14 11:24:34 +0200

    OMDB API and stuff

--- Movie4Life/src/hooks/useFetchMovies.js
+++ Movie4Life/src/hooks/useFetchMovies.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
 import axios from 'axios';
 
 export const useFetchMovies = () => {
@@ -9,6 +10,22 @@ export const useFetchMovies = () => {
     useEffect(() => {
         setIsLoading(true);
         axios.get('https://santosnr6.github.io/Data/favoritemovies.json')
+            .then(response => setMovies(response.data))
+            .catch(() => setIsError(true))
+            .finally(() => setIsLoading(false));
+    }, []);
+
+    return { movies, isLoading, isError };
+}
+
+export const useFetchSearch = ({string}) => {
+    const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
+
+    useEffect(() => {
+        setIsLoading(true);
+        axios.get(`http://www.omdbapi.com/?apikey=bb97d4a3&s=${string}`)
             .then(response => setMovies(response.data))
             .catch(() => setIsError(true))
             .finally(() => setIsLoading(false));
--- Movie4Life/src/pages/SearchPage/SearchPage.jsx
+++ Movie4Life/src/pages/SearchPage/SearchPage.jsx
@@ -1,4 +1,25 @@
+import { useEffect, useState } from "react";
+import { useFetchSearch } from "../../hooks/useFetchMovies"
+import { useParams } from "react-router-dom";
+
 const SearchPage = () => {
+  const Test = useParams();
+  const [search, setSearch] = useState('');
+  const { movies, isLoading, isError } = useFetchSearch(search);
+
+  useEffect(() => {
+      if (Test) {
+        setSearch(Test);
+        
+      }
+  }, [Test]);
+
+  console.log(search);
+  console.log(movies);
+
+  if (isLoading) return <div>Laddar filmer...</div>
+  if (isError) return <div>Något gick fel.</div>;
+
   return (
     <div>SearchPage</div>
   )
