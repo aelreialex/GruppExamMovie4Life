3de7fe61ed696f50536ba8b6a02eb6482be6492a
Boss <chaiyapat_ass@hotmail.com>
2026-04-15 15:21:23 +0200

    Watchlist Page and other stuff

--- Movie4Life/src/pages/LandingPage/LandingPage.jsx
+++ Movie4Life/src/pages/LandingPage/LandingPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useFetchMovies } from "../../hooks/useFetchMovies";
 import { shuffleArray } from "../../utils/shuffleArray";
+import { useWatchlist } from "../../hooks/useWatchlist";
 import MovieCard from "../../components/MovieCard/MovieCard";
 import "./landingPage.css"
 
@@ -16,11 +17,17 @@ const LandingPage = () => {
     }
   }, [movies]); 
 
-  if (isLoading) return <div>Laddar filmer...</div>
-  if (isError) return <div>Något gick fel.</div>;
-
   console.log(movieList)
 
+  const { watchlist, addToWatchlist, removeFromWatchlist } = useWatchlist();
+
+  const isOnWatchlist = (id) => {
+      if(watchlist.some(movie => movie.imdbID === id)) return true;
+      else return false;
+  }
+
+  if (isLoading) return <div>Laddar filmer...</div>
+  if (isError) return <div>Något gick fel.</div>;
 
   return (
     <div className="wrapper">
@@ -28,7 +35,7 @@ const LandingPage = () => {
         <h2 className="landingPage__recommended">Recommended Movies</h2>
         <div className="landingPage__movies">
           {movieList.map((movie, index) => (
-            <MovieCard key={index} movie={movie}/> 
+            <MovieCard key={index} movie={movie} watchlist={watchlist} addToWatchlist={addToWatchlist} removeFromWatchlist={removeFromWatchlist} isOnWatchlist={isOnWatchlist}/> 
           ))}
         </div>
       </section>
